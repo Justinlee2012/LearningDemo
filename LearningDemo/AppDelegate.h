d5ff7c399d978b88359adce0fcb6ed7b5f74aa64
justinlee2012 <justinlee_2012@qq.com>
2015-09-10 10:39:28 +0800

    Initial Commit

---
+++ LearningDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearningDemo
+//
+//  Created by 李 俊 on 15/9/10.
+//  Copyright (c) 2015年 李 俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearningDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningDemo
+//
+//  Created by 李 俊 on 15/9/10.
+//  Copyright (c) 2015年 李 俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
